9b549a91758c7cc8890723f33b3c8414d6ff943d
Кащеев Илья <tactical949@gmail.com>
2021-03-28 20:26:17 +0600

    достаем время трека

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 count = document.querySelectorAll(".audio_row");
-var end = count.length
-for(i = 0; i < info.length; i++) {
-let str = new String
-srt = count[i].getAttribute("data-audio")
+for(i = 0; i < count.length; i++) {
+let str = new String;
+srt = count[i].getAttribute("data-audio");
+var arg = srt.split(',"content_id"'); //первый раз разбиваем строку
+var arg_2 = arg[0].split(':'); //второй раз рабиваем и получаем длину трека последним элементом
+var time = Number.parseInt(arg_2[arg_2.length - 1]); //достаем длину трека
 }
