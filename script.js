d2012d9e10f037d30ba2f1cfa84bbabf7f0ac8e4
Кащеев Илья <tactical949@gmail.com>
2021-03-28 20:52:21 +0600

    Добавил подсчет общего времени

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+var sum_time = 0;
+
 count = document.querySelectorAll(".audio_row");
 for(i = 0; i < count.length; i++) {
 let str = new String;
@@ -5,4 +7,6 @@ srt = count[i].getAttribute("data-audio");
 var arg = srt.split(',"content_id"'); //первый раз разбиваем строку
 var arg_2 = arg[0].split(':'); //второй раз рабиваем и получаем длину трека последним элементом
 var time = Number.parseInt(arg_2[arg_2.length - 1]); //достаем длину трека
+sum_time += time;
 }
+
